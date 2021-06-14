bebece36c820ebf10ac38eb4bdcac72014de3602
Rafael Tavares <rafaellelis@gmail.com>
2021-06-14 14:56:36 -0300

    acrescetando parametrização de quantidade de casas por linha

--- site.js
+++ site.js
@@ -3,6 +3,7 @@ function clickStart() {
   const qtdCartelas = parseInt(document.querySelector("#qtdCartelas").value, 0);
   const nomeBingo = document.querySelector("#nomeBingo").value;
   const imgCartela = document.querySelector("#imgCartela").value;
+  const quantidadeCasasLinha = document.querySelector("#quantidadeCasasLinha").value;
   const tipoBingo = parseInt(
     document.querySelector('input[name="tipoBingo"]:checked').value
   );
@@ -12,15 +13,20 @@ function clickStart() {
     return;
   }
 
-  const gen = new Gerador(listaTotal, imgCartela, tipoBingo);
+  if (!quantidadeCasasLinha || quantidadeCasasLinha < 3 || quantidadeCasasLinha > 5) {
+    alert("O Bingo deve ter entre 3 e 5 casas por linha!");
+    return;
+  }
+
+  const gen = new Gerador(listaTotal, imgCartela, tipoBingo, quantidadeCasasLinha);
   const out = document.querySelector("#output");
   out.className = "";
   out.innerHTML = "";
   out.classList.add(tipoBingo === 1 ? "bingo-num" : "bingo-text");
 
   for (let i = 0; i < qtdCartelas; i++) {
-    var t = new Cartela(nomeBingo, gen.GerarCartela());
-    out.append(t.genNode());
+    var t = new Cartela(nomeBingo, gen.GerarCartela(), quantidadeCasasLinha);
+    out.append(t.genNode(quantidadeCasasLinha, tipoBingo));
   }
 }
 
@@ -51,7 +57,7 @@ function getRandomInt(min, max) {
 }
 
 class Gerador {
-  constructor(lista, imagem, tipoBingo) {
+  constructor(lista, imagem, tipoBingo, quantidadeItensLinha = 5) {
     this.max = lista.length - 1;
     this.listaBase = [];
     this.tipoBingo = tipoBingo;
@@ -60,6 +66,7 @@ class Gerador {
     }
     imagem = imagem ? imagem : "passaros.png";
     this.casaImagem = new CasaCartela(null, imagem);
+    this.quantidadeItensLinha = quantidadeItensLinha
   }
 
   GerarCartela = function () {
@@ -68,8 +75,12 @@ class Gerador {
     let retorno = [];
     let j;
 
-    while (i < 25) {
-      if (i == 12) {
+    const quantidadeCasas = Math.pow(this.quantidadeItensLinha, 2)
+    const metade = Math.floor(quantidadeCasas/2)
+    const semImagem = quantidadeCasas % 2 === 0
+
+    while (i < quantidadeCasas) {
+      if (i == metade && !semImagem) {
         i++;
         continue;
       }
@@ -91,43 +102,63 @@ class Gerador {
       );
     }
     // incluir a casa da imagem no centro da cartela
-    retorno.splice(12, 0, this.casaImagem);
+    if(!semImagem){
+      retorno.splice(metade, 0, this.casaImagem);
+    }
     return retorno;
   };
 }
 
 class Cartela {
-  constructor(_titulo, _casas) {
+  constructor(_titulo, _casas, _quantidadeCasasLinha) {
     this.titulo = _titulo;
-    this.casas = [[], [], [], [], []];
+    this.casas = [];
+    const qtdCasasLinha = parseInt(_quantidadeCasasLinha, 10)
+    for(let a = 0; a < qtdCasasLinha; a++){
+      this.casas.push([])
+    }
+    
     let i = 0;
     let j = 0;
     // cria a matriz bidimensional transposta para ordenar corretamente de cima para baixo
-    for (let y = 0; y < 5; y++) {
-      for (let x = y, c = 0; c < 5; x += 5, c++) {
+    for (let y = 0; y < qtdCasasLinha; y++) {
+      for (let x = y, c = 0; c < qtdCasasLinha; x += qtdCasasLinha, c++) {
         this.casas[i][j++] = _casas[x];
-        if (j > 4) {
+        if (j > (qtdCasasLinha - 1)) {
           i++;
           j = 0;
         }
 
-        if (i > 4) break;
+        if (i > (qtdCasasLinha - 1)) break;
       }
     }
   }
 }
 
-Cartela.prototype.genNode = function () {
+Cartela.prototype.genNode = function (quantidadeCasasLinha, tipoBingo) {
   let container = document.createElement("div");
   container.classList.add("cartela");
+  const qtdCasasLinha = parseInt(quantidadeCasasLinha, 10)
+  let alturaLinha = 9 - qtdCasasLinha
+
+  if(tipoBingo === 2){
+    if(qtdCasasLinha === 3){
+      alturaLinha = 11
+    } else if(qtdCasasLinha === 4){
+      alturaLinha = 8.5
+    } else {
+      alturaLinha = 7
+    }
+  }
+
   container.innerHTML = `
         <h2 class="cartela-titulo">${this.titulo} </h2>
             <div class ="cartela-corpo">
             ${this.casas
               .map(
                 (item, i) => `
-                <div class="cartela-linha">
-                    ${item.map((casa, j) => `${casa.genTemplate()} `).join("")}
+                <div class="cartela-linha" style="height: calc(${alturaLinha}vw - 3px);">
+                    ${item.map((casa, j) => `${casa.genTemplate(quantidadeCasasLinha)} `).join("")}
                 </div>
             `
               )
@@ -143,10 +174,11 @@ class CasaCartela {
   }
 }
 
-CasaCartela.prototype.genTemplate = function () {
-  if (this.tipo == 1) return `<div class="cartela-casa">${this.display} </div>`;
+CasaCartela.prototype.genTemplate = function (_quantidadeCasasLinha) {
+  const qtdCasasLinha = parseInt(_quantidadeCasasLinha, 10)
+  if (this.tipo == 1) return `<div class="cartela-casa" style="max-width: ${100/qtdCasasLinha}%; width: ${100/qtdCasasLinha}%;">${this.display} </div>`;
   else
-    return `<div class ="cartela-casa cartela-casa-img"><img src='${this.display}' /></div>`;
+    return `<div class ="cartela-casa cartela-casa-img" style="max-width: ${100/qtdCasasLinha}%; width: ${100/qtdCasasLinha}%;"><img src='${this.display}' /></div>`;
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
